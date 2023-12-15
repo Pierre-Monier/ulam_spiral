8e2b2835ac2f7296a4fb5b75310771ce739ec9d5
Pierre-Monier <pierre.monier.dev@gmail.com>
2023-12-15 13:59:41 +0100

    feat: draw numbers with wrong coordinates

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 class UlamSpiral extends CustomPainter {
-  static const pointSize = Size(10, 10);
+  static const pointSize = Size(100, 100);
 
   @override
   void paint(Canvas canvas, Size size) {
+    var n = 0;
+
     for (var y = 0.0; y <= size.height; y += pointSize.height) {
       for (var x = 0.0; x <= size.width; x += pointSize.width) {
-        canvas.drawRect(
-            Rect.fromCenter(center: Offset(x, y), width: 5, height: 5),
-            Paint()..color = Colors.amber);
+        final numberPaint = TextPainter(
+            text: TextSpan(
+                text: n.toString(),
+                style: const TextStyle(fontSize: 8, color: Colors.amber)),
+            textDirection: TextDirection.ltr,
+            textAlign: TextAlign.center);
+
+        numberPaint.layout();
+        numberPaint.paint(canvas, Offset(x, y));
+
+        n++;
       }
     }
   }
