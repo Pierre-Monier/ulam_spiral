943439379f2b85e3ac53ea72c3b5869cce7d1fda
Pierre-Monier <pierre.monier.dev@gmail.com>
2023-12-15 13:40:59 +0100

    feat: drawing point

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class UlamSpiral extends CustomPainter {
+  static const pointSize = Size(10, 10);
+
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawRect(Rect.fromLTRB(0, 0, size.width, size.height),
-        Paint()..color = Colors.amber);
+    for (var y = 0.0; y <= size.height; y += pointSize.height) {
+      for (var x = 0.0; x <= size.width; x += pointSize.width) {
+        canvas.drawRect(
+            Rect.fromCenter(center: Offset(x, y), width: 5, height: 5),
+            Paint()..color = Colors.amber);
+      }
+    }
   }
 
   @override
